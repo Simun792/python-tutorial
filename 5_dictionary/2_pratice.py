684e5ca5e39237b791ee43acf6678a427bc295b0
Simun792 <goudasimanchal792@gmail.com>
2026-03-10 16:42:28 +0530

    add data

--- 5_dictionary/2_pratice.py
+++ 5_dictionary/2_pratice.py
@@ -13,7 +13,7 @@ s = {}
 print(type(s))'''
 
 '''6. Create an empty dictionary. Allow 4 friends to enter their favorite language as 
-value and use key as their names. Assume that the names are unique. '''
+value and use key as their names. Assume that the names are unique. 
 
 d = {}
 
@@ -33,4 +33,11 @@ name = input("Enter friends name: ")
 lang = input("Enter language name: ")
 d.update({name: lang})
 
-print(d)
+print(d) '''
+
+'''7. If the names of 2 friends are same; what will happen to the program in problem 
+6? '''
+
+'''9. Can you change the values inside a list which is contained in set S? '''
+
+s = {8, 7, 12, "Harry", [1,2]}
