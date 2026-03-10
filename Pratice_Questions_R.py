6152ec42c9b2ef3fa8412437e0fb1a5c756631b7
Simun792 <goudasimanchal792@gmail.com>
2026-03-10 22:11:48 +0530

    add this

---
+++ Pratice_Questions_R.py
@@ -0,0 +1,102 @@
+'''1. Write a Python program to store your name, age, and height and print them.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your hright: "))
+
+print("Name:", name)
+print("Age:", age)
+print("Height", height)'''
+
+#2. Write a program to add two integers entered by the user.
+
+'''a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+sum = a + b
+
+print("Sum =", sum)'''
+
+#3. Write a program to multiply a float and an integer.
+
+'''from operator import mul
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+mul = a * b 
+
+print("Mul: " , mul)'''
+
+# Convert a string "100" to integer and add 50 to it.
+
+# from unittest import result
+
+
+# a = "100"
+# # num = int(a)
+# # result = num + 50
+
+# # print(result)
+
+# print(int(a) + 50)
+
+#5. Write a program to check whether a number is greater than 100 (boolean result).
+
+# a = (99 > 100)
+# print(a)
+
+# num = int(input("Enter a number: "))
+# result = num > 100
+
+# print(result)
+
+#6. Write a program to concatenate two strings.
+
+# a = str(input("Enter your first_name: "))
+# b = str(input("Enter your last_name: "))
+
+# print(a + " " + b)
+
+#7. Write a program to find the length of a string.
+
+# a = ("Simun Gouda")
+
+# print(len(a))
+
+# a = input("Enter a string: ")
+
+# print("Lenth of the string is:", len(a))
+
+#8. Convert a float number to integer.
+
+# a = 5.0
+
+# print(int(a))
+
+# a = float(input("Enter a float number: "))
+
+# print(int(a))
+
+#9. Write a program to check if a number is even or odd.
+
+# a = int(input("Enter a number: "))
+
+# if a % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# temp = a
+# a = b 
+# b = temp
+
+# print("After swapping: ")
+# print("a =", a)
+# print("b =", b)
+
+#11. Create a list of 5 numbers and print it.
+
