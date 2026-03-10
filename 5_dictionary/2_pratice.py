f93e3c4e83e721bc7fb335a0ab011048a675fbd0
Simun792 <goudasimanchal792@gmail.com>
2026-03-10 16:21:27 +0530

    add this

--- 5_dictionary/2_pratice.py
+++ 5_dictionary/2_pratice.py
@@ -2,5 +2,35 @@
 s = set() 
 s.add(20) 
 s.add(20.0) 
-s.add('20') # length of s after these operations? '''
+s.add('20') # length of s after these operations? 
 
+print(len(s))
+
+5. s = {} 
+    What is the type of 's'? 
+
+s = {}
+print(type(s))'''
+
+'''6. Create an empty dictionary. Allow 4 friends to enter their favorite language as 
+value and use key as their names. Assume that the names are unique. '''
+
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+print(d)
