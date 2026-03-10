249f75b55f0947f8d6f8579f700821360679e00f
Simun792 <goudasimanchal792@gmail.com>
2026-03-10 10:27:22 +0530

    Update input.py

--- 2_variables and data types/input.py
+++ 2_variables and data types/input.py
@@ -2,6 +2,7 @@ a = int(input("Enter number 1: "))
 
 b = int(input("Enter number 2: "))
 
-print("Number a is: ", a)
-print("Number b is: ", b)
+print("Number a = ", a)
+print("Number b = ", b)
 print("Sum is ", a + b)
+print("Substraction is ", a - b)
