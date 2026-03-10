50df80bcde8ec9a6a65672ba855bcb8f45c9c6a3
Simun792 <goudasimanchal792@gmail.com>
2026-03-10 13:16:05 +0530

    deleted files

--- 05_dictionary/set_union_intersection.py
+++
@@ -1,6 +0,0 @@
-s1 = {12, 34, 48}
-s2 = {5, 16, 29, 12}
-
-print(s1.union(s2))
-print(s1.intersection(s2))
-
--- 05_dictionary/sets.py
+++
@@ -1,8 +0,0 @@
-e = set() # Dont use s = {} as it will create an empty dictionary
-
-s = {1, 23, 54, 5, 5, 5}
-
-print(s)
-
-
-
